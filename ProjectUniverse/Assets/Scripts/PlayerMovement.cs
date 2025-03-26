cc3ae3e569fd127d8e7436a105c917607fc73b79
DakotaCottman <djcottman@gmail.com>
2025-03-25 22:13:44 -0400

    camera stuff

--- ProjectUniverse/Assets/Scripts/PlayerMovement.cs
+++ ProjectUniverse/Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,6 @@
+using Unity.Cinemachine;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
@@ -9,20 +12,55 @@ public class PlayerMovement : MonoBehaviour
     private Vector3 _moveDir;
 
     public InputActionReference moveAction;
+    public InputActionReference jumpAction;
 
+    public float jumpForce;
 
+    [SerializeField] private Camera cam;
 
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+        
+        
+    }
+
+    
     private void Update()
     {
         Vector2 input = moveAction.action.ReadValue<Vector2>();
-        _moveDir = new Vector3(input.x, 0, input.y).normalized;
+        
+        
+        Vector3 cameraForward = cam.transform.forward;
+        Vector3 cameraRight = cam.transform.right;
+        
+        cameraForward.y = 0;
+        cameraForward.Normalize();
+    
 
-    }
-
-
-    private void FixedUpdate()
-    {
+        _moveDir = (cameraForward * input.y + cameraRight * input.x).normalized;
+        
         rb.linearVelocity = _moveDir * moveSpeed;
-            
+        rb.angularVelocity = Vector3.up * jumpForce;
     }
+
+    private void Jump(InputAction.CallbackContext context)
+    {
+        rb.AddForce(Vector3.up * jumpForce);
+        
+    }
+
+    private void OnEnable()
+    {
+        jumpAction.action.performed += Jump;
+    }
+
+    private void OnDisable()
+    {
+        jumpAction.action.performed -= Jump;
+    }
+
+
+   
 }
